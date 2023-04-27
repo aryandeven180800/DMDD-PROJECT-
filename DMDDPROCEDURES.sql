8d278d09b225170e646cfd156fa98e8bf39fd977
aryandeven180800 <114887600+aryandeven180800@users.noreply.github.com>
2023-04-27 17:02:41 -0400

    Add files via upload

---
+++ DMDDPROCEDURES.sql
@@ -0,0 +1,44 @@
+USE DMDDPROJECT
+Go
+Create Procedure [dbo].[spGetTestCountByEnrollmentID]
+@EnrollmentID int ,
+@TestCount int Output
+	As
+	Begin
+	Select @TestCount= count(*) 
+	from Tests 
+	where EnrollmentID= @EnrollmentID
+END
+Declare @TotalTestCount int
+Execute [spGetTestCountByEnrollmentID] '1001', @TotalTestCount output
+Print @TotalTestCount
+GO
+
+Create Procedure [dbo].[UpdateStudentTestScoreMarks3]
+@TestNo int,
+@Score decimal(5,2)
+As 
+Begin
+Update TestScore
+Set Score =@Score where TestNo=@TestNo
+--Print 'New Assigned score to the test no is : ' + cast(@Score as decimal)
+	End
+	EXEC [dbo].[UpdateStudentTestScoreMarks3] 27,34.0  ---CHANGES Score from assigned value
+
+	Select * from TestScore
+GO
+
+CREATE PROCEDURE ChangeProfessorname
+	(
+    @teachername varchar(50),
+	@teacherID int
+	)
+	As 
+	Begin
+	update Teachers
+	Set TeacherName = @teachername where TeacherID=@teacherID
+	--Print 'New Assigned Professor to the course is : ' + cast(@teachername as varchar)
+	End
+	EXEC ChangeProfessorname Maya,103  ---CHANGES Professor Name TO Pruthvi from assigned value
+
+	SELECT * FROM Teachers
---
+++ NONCLUSTEREDINDEX.sql
@@ -0,0 +1,25 @@
+USE DMDDPROJECT
+Go
+
+CREATE NONCLUSTERED INDEX [IX_NonClusteredIndex_Student_details]
+ON [dbo].[STUDENT] ([StudentID],[StudentName],[StudentEmail]);
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_NonClusteredIndex_Attendance]
+ON [dbo].[Attendance] ([ClassNo],[StudentID]);
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_NonClusteredIndex_Courses]
+ON [dbo].[Course] ([CourseID],[CourseName]);
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_NonClusteredIndex_Teachers]
+ON [dbo].[Teachers] ([TeacherID],[TeacherName],[TeacherEmail]);
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_NonClusteredIndex_Enrollment]
+ON [dbo].[Enrollment] ([EnrollmentID],[StudentID],[CourseID]);
